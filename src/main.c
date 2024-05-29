166f149933ada5d93f6e52227c75c07c87df1dc8
Mathius <info@yogimathius.dev>
2024-05-28 23:35:26 -0700

    feat: Add support for checking if a command is a built-in or executable

--- src/main.c
+++ src/main.c
@@ -4,6 +4,111 @@
 #include <string.h>
 #include <unistd.h>
 
+
+// List of built-in commands
+char* builtins[] = {
+    "alias",
+    "bg",
+    "bind",
+    "break",
+    "builtin",
+    "cd",
+    "command",
+    "compgen",
+    "complete",
+    "compopt",
+    "continue",
+    "declare",
+    "dirs",
+    "disown",
+    "echo",
+    "enable",
+    "eval",
+    "exec",
+    "exit",
+    "export",
+    "false",
+    "fc",
+    "fg",
+    "getopts",
+    "hash",
+    "help",
+    "history",
+    "jobs",
+    "kill",
+    "let",
+    "local",
+    "logout",
+    "mapfile",
+    "popd",
+    "printf",
+    "pushd",
+    "pwd",
+    "read",
+    "readarray",
+    "readonly",
+    "return",
+    "set",
+    "shift",
+    "shopt",
+    "source",
+    "suspend",
+    "test",
+    "times",
+    "trap",
+    "true",
+    "type",
+    "typeset",
+    "ulimit",
+    "umask",
+    "unalias",
+    "unset",
+    "wait",
+};
+
+int is_builtin(const char* command) {
+  size_t num_builtins = sizeof(builtins) / sizeof(builtins[0]);
+  for (size_t i = 0; i < num_builtins; i++) {
+    if (strcmp(command, builtins[i]) == 0) {
+      return 1; // Command is a built-in
+    }
+  }
+  return 0; // Command is not a built-in
+}
+
+char* is_executable(const char* command) {
+  char* path = getenv("PATH");
+  if (path == NULL) {
+    return NULL;
+  }
+
+  char* path_copy = strdup(path);
+  if (path_copy == NULL) {
+    return NULL;
+  }
+
+  char* token = strtok(path_copy, ":");
+  while (token != NULL) {
+    char* full_path = (char*)malloc(strlen(token) + strlen(command) + 2);
+    if (full_path == NULL) {
+      free(path_copy);
+      return NULL;
+    }
+
+    snprintf(full_path, strlen(token) + strlen(command) + 2, "%s/%s", token, command);
+    if (access(full_path, X_OK) == 0) {
+      free(path_copy);
+      return full_path;
+    }
+
+    free(full_path);
+    token = strtok(NULL, ":");
+  }
+
+  free(path_copy);
+  return NULL;
+}
+
 // void execute_command(char* command) {
 int execve(const char* filename, char* const argv[], char* const envp[]);
 
@@ -47,14 +152,15 @@ void open_shell() {
       pid_t pid = fork();
 
       if (pid == 0) {  // Child process
-        if (strcmp(args[0], "ls") == 0) {
-          execve("/bin/ls", args, NULL);
+        char* exec = is_executable(args[0]);
+        if (exec != NULL) {
+          execve(exec, args, NULL);
         }
-        else if (strcmp(args[0], "cat") == 0) {
-          execve("/bin/cat", args, NULL);
-        }
-        else if (strcmp(args[0], "sh") == 0) {
-          execve("/bin/sh", args, NULL);
+
+        else if (is_builtin(args[0])) {
+          char path[1024];
+          snprintf(path, 1024, "/bin/%s", args[0]);
+          execve(path, args, NULL);
         }
         else {
           printf("Command not found: %s\n", args[0]);
