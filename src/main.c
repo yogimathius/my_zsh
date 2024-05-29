1ec4bfaf05a85dfa2709043583b9d0148626d171
Mathius <info@yogimathius.dev>
2024-05-28 23:04:15 -0700

    text: use fgets, don't exit in exit

--- src/main.c
+++ src/main.c
@@ -13,19 +13,24 @@ void open_shell() {
 
   while (!shell_exited) {
     char* input = (char*)malloc(1024);
-    printf("$ ");
-    scanf("%s", input);
     if (input == NULL) {
+      fprintf(stderr, "Memory allocation failed\n");
+      exit(1);
+    }
+
+    printf("$ ");
+    if (fgets(input, 1024, stdin) == NULL) {
+      free(input);
       break;
     }
+    input[strcspn(input, "\n")] = 0;
     if (strlen(input) > 0) {
       // add_history(input);
-      printf("You entered: %s\n", input);
+      // printf("You entered: %s\n", input);
     }
     if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
-      printf("Exiting shell...\n");
+      // printf("Exiting shell...\n");
       shell_exited = 1;
-      exit(0);
     }
     else {
       // execute_command(input);
