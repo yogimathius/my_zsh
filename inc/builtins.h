7b071a2c045ef72dc7b7659f8adda5a7db10e4ff
Mathius <info@yogimathius.dev>
2024-06-29 22:27:31 -0700

    feat: maybe complete custom_setenv? fix up custom_echo to handle envs

--- inc/builtins.h
+++ inc/builtins.h
@@ -4,10 +4,9 @@
 int custom_cd(char** args);
 int custom_exit(char** args);
 int custom_env(char** args, char** env);
-int custom_echo(char** args);
-int custom_ls(char** args);
+int custom_echo(char** args, char** env);
 int custom_pwd(char** args);
-int custom_setenv(char** args);
+int custom_setenv(char** args, char** env);
 int custom_unsetenv(char** args);
 int custom_which(char** args);
 
--- src/builtins.c
+++ src/builtins.c
@@ -47,32 +47,25 @@ int custom_env(char** args, char** env) {
   return 0;
 }
 
-int custom_echo(char** args) {
+int custom_echo(char** args, char** env) {
+  if (args[1][0] == '$') {
+    int i = 0;
+    while (env != NULL && env[i] != NULL) {
+      if (strncmp(env[i], args[1] + 1, strlen(args[1]) - 1) == 0) {
+        strtok(env[i], "=");
+        char* value = strtok(NULL, "=");
+        printf("%s\n", value);
+        return 0;
+      }
+      i++;
+    }
+    return 0;
+  }
   for (int i = 1; args[i] != NULL; i++) {
     printf("%s ", args[i]);
   }
   printf("\n");
-  return 1;
-
-}
-
-int custom_ls(char** args) {
-  UNUSED(args);
-  const char* path = ".";
-  DIR* dir = opendir(path);
-  if (dir == NULL) {
-    perror("ls");
-    return 0;
-  }
-
-  struct dirent* entry;
-  while ((entry = readdir(dir)) != NULL) {
-    printf("%s\n", entry->d_name);
-  }
-
-  closedir(dir);
   return 0;
-
 }
 
 int custom_pwd(char** args) {
@@ -84,19 +77,38 @@ int custom_pwd(char** args) {
   }
   printf("%s\n", path);
   return 0;
-
-
 }
 
-int custom_setenv(char** args) {
-  if (args[1] == NULL || args[2] == NULL) {
-    fprintf(stderr, "setenv: expected two arguments\n");
+int custom_setenv(char** args, char** env) {
+  if (args[1] == NULL) {
+    fprintf(stderr, "setenv: expected argument\n");
   }
-  else {
-    if (setenv(args[1], args[2], 1) != 0) {
-      perror("setenv");
+  char* env_var = strtok(args[1], "=");
+  char* value = strtok(NULL, "=");
+  if (env_var == NULL) {
+    fprintf(stderr, "setenv: expected argument in the form of VAR=VAL\n");
+  }
+  char* arg = value + 2;
+  // slice off #{ and closing } that is in arg currently
+  arg[strlen(arg) - 1] = '\0';
+
+  int i = 0;
+  // combine the env_var and arg
+  char* new_env = (char*)malloc(strlen(arg) + strlen(env_var) + 2);
+  if (new_env == NULL) {
+    perror("malloc");
+    return 1;
+  }
+  snprintf(new_env, strlen(env_var) + strlen(arg) + 2, "%s=%s", env_var, arg);
+  while (env[i] != NULL) {
+    if (strncmp(*env, env_var, strlen(env_var)) == 0) {
+      *env = strdup(new_env);
+      return 0;
     }
+    i++;
   }
+
+  env[i] = strdup(new_env);
   return 0;
 
 
@@ -143,16 +155,13 @@ int run_builtin(char** args, char** env) {
     return custom_env(args, env);
   }
   else if (strcmp(args[0], "echo") == 0) {
-    return custom_echo(args);
-  }
-  else if (strcmp(args[0], "ls") == 0) {
-    return custom_ls(args);
+    return custom_echo(args, env);
   }
   else if (strcmp(args[0], "pwd") == 0) {
     return custom_pwd(args);
   }
   else if (strcmp(args[0], "setenv") == 0) {
-    return custom_setenv(args);
+    return custom_setenv(args, env);
   }
   else if (strcmp(args[0], "unsetenv") == 0) {
     return custom_unsetenv(args);
